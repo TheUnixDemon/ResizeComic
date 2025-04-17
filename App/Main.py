099df6390ff6504bd08c701fe09a02a66415dfd2
TheUnixDaemon <theunixdaemon@gmail.com>
2025-04-17 22:54:57 +0200

    Path and Main tested & debugged

--- App/Main.py
+++ App/Main.py
@@ -1,4 +1,11 @@
 from Argument import Argument
 from Resources.Path import Path
 
-agrument: Argument = Argument()
+# argument variables
+#argument: Argument = Argument()
+#targetDir: str = argument.getPath()
+targetDir: str = "./Compressed" # only for testing, afterwards its argument.getPath()
+
+# paths to the internal files of targetDir
+path: Path = Path(targetDir)
+print(path.getPaths())
--- App/Resources/Path.py
+++ App/Resources/Path.py
@@ -3,24 +3,25 @@ import os
 class Path:
     def __init__(self, targetDir: str):
         self.__targetDir: str = targetDir
+        self.__checkDir()
 
     # returns all paths to files in targetDir
     def getPaths(self) -> dict[str, str]:
         try:
-            paths: dict[str, str] = {}
+            paths: list[str] = []
             for root, dirs, files in os. walk(self.__targetDir):
                 for file in files:
-                    sourcePath: str = os.path.join(root, file)
-                    paths[sourcePath] = sourcePath.replace(self.__targetDir, self.__targetDir)
+                    path: str = os.path.join(root, file)
+                    paths.append(path)
             return paths
         except Exception as e:
             print(f"Error occurred: {e}")
 
     # checks if passed directory can be found
-    def checkDir(self):
+    def __checkDir(self):
         try:
             if not os.path.exists(self.__targetDir):
-                raise f"Directory *{self.__sourceDirectory}* not found"
+                raise f"Directory *{self.__targetDir}* not found"
         except Exception as e:
             print(f"Error occurred: {e}")
             exit()
